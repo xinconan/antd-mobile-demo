d33b7127d066097cc72316c7881978cd375aa6fd
xinconan <xinconan2@sina.com>
2017-08-01 20:33:43 +0800

    目录搭建

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # antd-mobile-demo
 antd-mobile项目搭建demo
+
+# 开发环境
+Node: 6.10.x  
+npm: 4.x
+
+# 目录结构
+```
+│  package.json  
+│  README.md  
+│  webpack.config.dev.js  
+│  webpack.config.js  
+│  
+├─public  
+│  ├─dist  
+│  ├─images  
+│  └─js  
+└─source  
+    ├─react  
+    │  ├─common  通用组件
+    │  └─home  首页
+    │      ├─components  组件（当前页面使用）
+    │      └─pages  子页面
+    └─sass  
+
+```
+
+# tips
+安装node-sass的时候，经常会由于网络问题下载不下来（尽管npm源设置成淘宝了），新建一个`.npmrc`文件，内容如下，参考地址：[http://www.cnblogs.com/savokiss/p/6474673.html](http://www.cnblogs.com/savokiss/p/6474673.html)：
+```
+sass_binary_site=https://npm.taobao.org/mirrors/node-sass/
+registry=https://registry.npm.taobao.org
+```
---
+++ webpack.config.dev.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+//svg
+const svgDirs = [
+  require.resolve('antd-mobile').replace(/warn\.js$/, ''), // antd-mobile 内置svg
+  //path.resolve(__dirname, 'src/my-project-svg-foler'),  // 业务代码本地私有 svg 存放目录
+];
+
+module.exports = {
+
+    externals: {
+        'jquery': '$',
+        'fastclick': 'FastClick',
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'react-router': 'ReactRouter'
+    },
+
+    resolve: {
+        modules: [
+            path.resolve(__dirname, 'node_modules'),
+            path.join(__dirname, './source')
+        ],
+        extensions: ['.web.tsx', '.web.ts', '.web.jsx', '.web.js', '.ts', '.tsx', '.js', '.jsx', '.json'],
+    },
+
+    devtool: "source-map",
+
+    module: {
+        rules: [
+            {
+                test: /\.js[x]?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            plugins: [
+                                ["transform-runtime", {polyfill: false}],
+                                ["import", [{"style": "css", "libraryName": "antd-mobile"}]]
+                            ],
+                            presets: ['es2015', 'stage-0', 'react']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.html$/,
+                use: ['html-loader']
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader',
+                            'postcss-loader'
+                        ]
+                    }
+                )
+            },
+            {
+                test: /\.scss$/,
+                loader: ExtractTextPlugin.extract({
+                        fallback: 'style-loader',
+                        use: [
+                            'css-loader',
+                            'postcss-loader',
+                            'sass-loader'
+                        ]
+                    }
+                )
+            },
+            {
+                test: /\.(png|jpg|jpeg|gif)$/i,
+                use: ['url-loader?limit=8192']
+            },
+            {
+                test: /\.(svg)$/i,
+                use: ['svg-sprite-loader'],
+                include: svgDirs
+            },
+            {
+                test: /\.(eot|woff|ttf)$/,
+                use: ['file-loader']
+            }
+        ],
+    },
+
+    
+};
